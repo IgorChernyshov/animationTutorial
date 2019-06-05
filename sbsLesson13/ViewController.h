7cf0f864b1103185bb91e3b2b1e1ee3266266909
Igor Chernyshov <ichernyshov@bk.ru>
2019-06-05 18:38:48 +0300

    Initial Commit

---
+++ sbsLesson13/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sbsLesson13
+//
+//  Created by Igor Chernyshov on 05/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sbsLesson13/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sbsLesson13
+//
+//  Created by Igor Chernyshov on 05/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
